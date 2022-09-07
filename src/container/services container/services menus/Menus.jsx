6bf2732c4c0e5aef5c1bcce5f920412b835418c1
vnz_ <mahmoudnasef2030@gmail.com>
2022-09-07 11:11:20 +0200

    services

--- src/container/services container/services menus/Menus.jsx
+++ src/container/services container/services menus/Menus.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import './Menus.css'
 function Menus() {
   return (
-    <section id="menus" className='menus-background'>
-      <div className='menus-wrapper'>
-        <div className="menus-content">
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
+    <section id="menus" className='menus-background h-[100vh] '>
+      <div className='menus-wrapper h-full p-8 lg:p-16'>
+        <div className="menus-content flex flex-col h-full justify-center">
+          <h3 className='mt-8'><a href="/Bar" className=' text-white text-[35px] lg:text-[45px] hover:underline p-Cormorant'>Bar Menu</a></h3>
+          <h3 className='mt-8'><a href="/Food" className='text-white text-[35px] lg:text-[45px] hover:underline p-Cormorant'>Food Menu</a></h3>
+          <h3 className='mt-8'><a href="/Dessert" className='text-white text-[35px] lg:text-[45px] hover:underline p-Cormorant'>Desserts Menu</a></h3>
         </div>
       </div>
     </section>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,6 +3,8 @@ import NavBar from '../components/Navbar/Navbar'
 import AboutHeader from '../container/About container/About header/AboutHeader'
 import ServiceQuality from '../container/services container/service quality/ServiceQuality'
 import Menus from '../container/services container/services menus/Menus'
+import BookTable from '../container/BarContainers/bookTable/BookTable'
+import HappyHours from '../container/BarContainers/happy Hours/HappyHours'
 function Services() {
   return (
     <>
@@ -12,6 +14,8 @@ function Services() {
     />
     <ServiceQuality />
     <Menus />
+    <BookTable />
+    <HappyHours />
     </>
   )
 }
